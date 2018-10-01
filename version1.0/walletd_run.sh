eb08dc0ee39720a9db5d341047baad895aa4ebb2
Fork TIPS <jojapoppa@protonmail.com>
2018-10-01 05:03:40 +0000

    document api's

--- version1.0/walletd_run.sh
+++ version1.0/walletd_run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo
-echo "Documentation on the rpc interface for this wallet is at: http://forknote.net/documentation/rpc-wallet/rpc-wallet-curl-examples"
-echo
 echo "fedoragold_walletd is now running..."
+nohup ./fedoragold_daemon --log-level 0 &
 ./fedoragold_walletd --container-file walletdfile --container-password joja4 --log-level 0 --bind-address 127.0.0.1 --bind-port 9090 --p2p-bind-port 30158 --daemon-address 127.0.0.1 --daemon-port 30159
