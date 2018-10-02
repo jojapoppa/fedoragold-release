3b78390cd1991dfcd54c9958670baf7b841c05d4
Fork TIPS <jojapoppa@protonmail.com>
2018-10-02 06:03:55 +0000

    next build...

--- version1.0/walletd_run.sh
+++ version1.0/walletd_run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo
 echo "fedoragold_walletd is now running..."
-nohup ./fedoragold_daemon --log-level 0 &
-./fedoragold_walletd --container-file walletdfile --container-password joja4 --log-level 0 --bind-address 127.0.0.1 --bind-port 9090 --p2p-bind-port 30158 --daemon-address 127.0.0.1 --daemon-port 30159
+#nohup ./fedoragold_daemon --log-level 0 &
+./fedoragold_walletd --local --container-file walletdfile --container-password joja4 --log-level 0 --bind-port 9090 
+# --bind-address 127.0.0.1 --p2p-bind-port 30158 --daemon-address 127.0.0.1 --daemon-port 30159
