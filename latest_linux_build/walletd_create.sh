6ea9cca5e347632de39647f749de72d5418adfac
jojapoppa <jojapoppa@protonmail.com>
2019-11-13 15:15:42 -0800

    doc updates...

--- latest_linux_build/walletd_create.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./fedoragold_walletd --container-file walletdfile --container-password joja4 --generate-container
--- latest_linux_build/walletd_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo
-echo "fedoragold_walletd is now running..."
-./fedoragold_walletd --container-file walletdfile --container-password joja4 --log-level 2 --bind-address 127.0.0.1 --bind-port 9090 --daemon-address 127.0.0.1 --daemon-port 30159
